42c64e970827993765442f0dac3f8b489ebfe4fb
Pan Zhiyong <pan.zhiyong@cybertech.com.cn>
2015-02-06 11:18:09 +0800

    limit max bmp size to avoid out-of-memory.

--- library/src/ru/truba/touchgallery/TouchView/FileTouchImageView.java
+++ library/src/ru/truba/touchgallery/TouchView/FileTouchImageView.java
@@ -21,14 +21,17 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.util.AttributeSet;
+import ru.truba.touchgallery.TouchView.InputStreamWrapper.InputStreamProgressListener;
+
 import java.io.File;
 import java.io.FileInputStream;
 
-import ru.truba.touchgallery.TouchView.InputStreamWrapper.InputStreamProgressListener;
-
 public class FileTouchImageView extends UrlTouchImageView 
 {
-	
+    // limit max bmp size to avoid out-of-memory.
+    private final static int MAX_WIDTH = 1280;
+    private final static int MAX_HEIGHT = 720;
+
     public FileTouchImageView(Context ctx)
     {
         super(ctx);
@@ -63,12 +66,47 @@ public class FileTouchImageView extends UrlTouchImageView
 						publishProgress((int)(progressValue * 100));
 					}
 				});
-                bm = BitmapFactory.decodeStream(bis);
+
+                // First decode with inJustDecodeBounds=true to check dimensions
+                final BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inJustDecodeBounds = true;
+                BitmapFactory.decodeFile(path, options);
+
+                // Calculate inSampleSize
+                options.inSampleSize = calculateInSampleSize(options, MAX_WIDTH, MAX_HEIGHT);
+
+                // Decode bitmap with inSampleSize set
+                options.inJustDecodeBounds = false;
+                bm = BitmapFactory.decodeStream(bis, null, options);
+
                 bis.close();
             } catch (Exception e) {
                 e.printStackTrace();
             }
             return bm;
         }
+    }
+
+    private static int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) > reqHeight
+                    && (halfWidth / inSampleSize) > reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
     }
 }
