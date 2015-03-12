3c963a1bd25e4a0d4990089388eca4b0bcc47e86
Pan Zhiyong <pan.zhiyong@cybertech.com.cn>
2015-03-12 10:38:15 +0800

    fix: NullPointerException by removing GalleryFileActivity

--- example/src/ru/truba/touchgallery/MainActivity.java
+++ example/src/ru/truba/touchgallery/MainActivity.java
@@ -1,17 +1,19 @@
 package ru.truba.touchgallery;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import android.os.Bundle;
 import android.app.ListActivity;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends ListActivity {
 	
@@ -56,12 +58,48 @@ public class MainActivity extends ListActivity {
 		{
 		case 0:
 			i = new Intent(this, GalleryUrlActivity.class);
+			startActivity(i);
 			break;
 		case 1:
-			i = new Intent(this, GalleryFileActivity.class);
+			startTouchGalleryActivity();
 			break;
 		}
-		startActivity(i);
 	}
-	
+
+	private void startTouchGalleryActivity() {
+		ArrayList<String> items = new ArrayList<String>();
+		try {
+			String[] urls = getAssets().list("");
+
+			for (String filename : urls)
+			{
+				if (filename.matches(".+\\.(jpg|png)"))
+				{
+					String path = getFilesDir() + "/" + filename;
+					copy(getAssets().open(filename), new File(path) );
+					items.add(Uri.fromFile(new File(path)).toString());
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		startActivity(new Intent(this, TouchGalleryActivity.class)
+				.putStringArrayListExtra(TouchGalleryActivity.EXTRA_URLS, items)
+				.putExtra(TouchGalleryActivity.EXTRA_POSITION, 0));
+	}
+
+	public void copy(InputStream in, File dst) throws IOException {
+
+		OutputStream out = new FileOutputStream(dst);
+
+		// Transfer bytes from in to out
+		byte[] buf = new byte[1024];
+		int len;
+		while ((len = in.read(buf)) > 0) {
+			out.write(buf, 0, len);
+		}
+		in.close();
+		out.close();
+	}
 }
